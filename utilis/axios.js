2ca37204e3c3164b4d746232ee95280d6bbbe02c
Saif-Ul-llah <saifhammad411@gmail.com>
2023-12-13 02:23:43 +0500

    node_err

--- utilis/axios.js
+++ utilis/axios.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://cute-cyan-lion-cape.cyclic.app',
 });
 
 export default instance; 
